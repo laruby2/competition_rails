02a85ec308f4a01c207319a150222a98e2ab48ed
Kang-Kyu Lee <kangkyu1111@gmail.com>
2022-08-21 08:05:13 -0700

    Add styles for three pages

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,8 @@
 module ApplicationHelper
+  def class_name_by_tab(path)
+    if current_page?(path)
+      return "bg-gray-900 text-white px-3 py-2 rounded-md text-sm font-medium"
+    end
+    "text-gray-300 hover:bg-gray-700 hover:text-white px-3 py-2 rounded-md text-sm font-medium"
+  end
 end
