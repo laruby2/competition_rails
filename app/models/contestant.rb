b403c9fdb6de8cf9bc6dc0b0b686ae061296c19d
Kang-Kyu Lee <kangkyu1111@gmail.com>
2023-03-05 10:45:14 -0800

    Add & remove automatically on audience contestants page

--- app/models/contestant.rb
+++ app/models/contestant.rb
@@ -2,4 +2,15 @@ class Contestant < ApplicationRecord
   has_many :votes, dependent: :destroy
 
   attr_accessor :my_vote
+
+  after_create_commit -> {
+    broadcast_append_to "audience_contestants",
+      target: "audience_contestant",
+      partial: "contestants/contestant",
+      locals: { contestant: self }
+  }
+
+  after_destroy_commit -> {
+    broadcast_remove_to "audience_contestants"
+  }
 end
