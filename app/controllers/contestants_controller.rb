bd93459404751e5cafb7441c8cb40f448e69a24f
Kang-Kyu Lee <kangkyu1111@gmail.com>
2023-03-05 15:34:37 -0800

    Remove TODO comments does not make much sense

--- app/controllers/contestants_controller.rb
+++ app/controllers/contestants_controller.rb
@@ -2,7 +2,7 @@ class ContestantsController < ApplicationController
   before_action :require_signin
 
   def index
-    @contestants = Contestant.all # TODO: all contestants of current round
+    @contestants = Contestant.all
     @contestants = current_user.assign_my_votes_to_contestants(@contestants)
   end
 end
