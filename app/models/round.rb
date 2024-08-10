281bb8265ef4fef779427a6cc32f9ef9eb5e5f5e
Kang-Kyu Lee <kangkyu1111@gmail.com>
2024-08-09 21:28:35 -0700

    Add 'how to share' a round

--- app/models/round.rb
+++ app/models/round.rb
@@ -1,4 +1,6 @@
 class Round < ApplicationRecord
+  include Rails.application.routes.url_helpers
+
   has_many :audiences, dependent: :destroy
   has_many :users, through: :audiences
   has_many :contestants
@@ -8,5 +10,13 @@ class Round < ApplicationRecord
 
   def to_param
     another_id
+  end
+
+  def url
+    round_url(self)
+  end
+
+  def self.default_url_options
+    ActionMailer::Base.default_url_options
   end
 end
